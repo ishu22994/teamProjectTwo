968436ecc635fd6e4b0d857a70b2c87da37c31fb
ishit22994 <ishitshah94@gmail.com>
2020-01-13 18:01:30 +0530

    DTO and Entity added

---
+++ src/main/java/com/example/teamProjectTwo/dto/EmployeeDTO.java
@@ -0,0 +1,44 @@
+package com.example.teamProjectTwo.dto;
+
+import java.util.Date;
+
+public class EmployeeDTO {
+
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    private double experience;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public double getExperience() {
+        return experience;
+    }
+
+    public void setExperience(double experience) {
+        this.experience = experience;
+    }
+}
+
