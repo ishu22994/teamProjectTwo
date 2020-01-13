c02fb26eb4ed0abaa5baa2e582a8d1c5382f3e36
swarnimgupta <guptaswarnim.14@gmail.com>
2020-01-13 18:42:54 +0530

    csv and xml read from file

--- src/main/java/com/example/teamProjectTwo/service/Impl/CsvFileHandlerServiceImpl.java
+++ src/main/java/com/example/teamProjectTwo/service/Impl/CsvFileHandlerServiceImpl.java
@@ -1,4 +1,61 @@
 package com.example.teamProjectTwo.service.Impl;
 
-public class CsvFileHandlerServiceImpl {
+import com.example.teamProjectTwo.dto.EmployeeDTO;
+import com.example.teamProjectTwo.service.MyFileHandler;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CsvFileHandlerServiceImpl implements MyFileHandler {
+
+    @Override
+    public  void read() {
+        String csvFile = "/Users/ishitshah/Downloads/employee.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] c = line.split(cvsSplitBy);
+                double experience = new Double(c[3]);
+                Date date = new SimpleDateFormat("MM/dd/yy").parse(c[2]);
+                EmployeeDTO employeeDTO = new EmployeeDTO();
+                employeeDTO.setFirstName(c[0]);
+                employeeDTO.setLastName(c[1]);
+                employeeDTO.setDateOfBirth(date);
+                employeeDTO.setExperience(experience);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void write(){
+
+    }
+
 }
--- src/main/java/com/example/teamProjectTwo/service/Impl/XmlFileHandlerServiceImpl.java
+++ src/main/java/com/example/teamProjectTwo/service/Impl/XmlFileHandlerServiceImpl.java
@@ -1,4 +1,56 @@
 package com.example.teamProjectTwo.service.Impl;
 
-public class XmlFileHandlerServiceImpl {
+import com.example.teamProjectTwo.dto.EmployeeDTO;
+import com.example.teamProjectTwo.service.MyFileHandler;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class XmlFileHandlerServiceImpl implements MyFileHandler {
+
+    public  void  read() {
+
+        try {
+            File file = new File("/Users/ishitshah/Downloads/employee.xml");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = null;
+
+            db = dbf.newDocumentBuilder();
+            Document doc = db.parse(file);
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeList = doc.getElementsByTagName("employee");
+            for (int iterator = 0; iterator < nodeList.getLength(); iterator++) {
+
+                Node node = nodeList.item(iterator);
+                Element element = (Element) node;
+
+
+                String firstName = element.getElementsByTagName("firstName").item(0).getTextContent();
+                String lastName = element.getElementsByTagName("lastName").item(0).getTextContent();
+                Date date = new SimpleDateFormat("MM/dd/yy").parse(element.getElementsByTagName("dateOfBirth").item(0).getTextContent());
+
+                double experience = new Double(element.getElementsByTagName("experience").item(0).getTextContent());
+                EmployeeDTO employee = new EmployeeDTO();
+                employee.setFirstName(firstName);
+                employee.setLastName(lastName);
+                employee.setDateOfBirth(date);
+                employee.setExperience(experience);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public  void write(){
+
+    }
 }
