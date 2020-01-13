d72f3fdef3bfe0a7ffd07ec897c31c62cbd31add
ishit22994 <ishitshah94@gmail.com>
2020-01-13 18:23:00 +0530

    All Files are Created

---
+++ src/main/java/com/example/teamProjectTwo/service/Impl/XmlFileHandlerServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.teamProjectTwo.service.Impl;
+
+public class XmlFileHandlerServiceImpl {
+}
---
+++ src/main/java/com/example/teamProjectTwo/service/XmlFileHandlerService.java
@@ -0,0 +1,4 @@
+package com.example.teamProjectTwo.service;
+
+public interface XmlFileHandlerService {
+}
