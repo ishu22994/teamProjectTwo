03d865519451ef344f5660031010ba16c13038ab
ishit22994 <ishitshah94@gmail.com>
2020-01-13 17:22:46 +0530

    First commit

---
+++ src/main/java/com/example/teamProjectTwo/TeamProjectTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamProjectTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProjectTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teamProjectTwo/TeamProjectTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamProjectTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
