129c5af946a32b614994edb10e77223533c00678
ishit22994 <ishitshah94@gmail.com>
2020-01-13 18:42:23 +0530

    All Files are Created

--- src/main/java/com/example/teamProjectTwo/service/Impl/JsonFileHandlerServiceImpl.java
+++ src/main/java/com/example/teamProjectTwo/service/Impl/JsonFileHandlerServiceImpl.java
@@ -1,4 +1,45 @@
 package com.example.teamProjectTwo.service.Impl;
 
+import com.example.teamProjectTwo.dto.EmployeeDTO;
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.*;
+import org.apache.tomcat.util.json.ParseException;
+
+import java.io.FileReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class JsonFileHandlerServiceImpl {
+
+    public  void  read() throws Exception {
+        String filename="/Users/ishitshah/Downloads/employee.json";
+        FileReader reader = new FileReader(filename);
+        JSONParser jsonParser = new JSONParser();
+        Object jsonData = jsonParser.parse(reader);
+        JSONArray jsonArray = (JSONArray) jsonData;
+        for(int i=0;i<jsonArray.size();i++) {
+            JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+            String firstName = (String) jsonObject.get("firstName");
+            String lastName = (String) jsonObject.get("firstName");
+            Date date = parseDate((String) jsonObject.get("dateOfBirth"), "MM/dd/yy");
+            long experiance = (long) jsonObject.get("experience");
+            EmployeeDTO employee = new EmployeeDTO();
+            employee.setFirstName(firstName);
+            employee.setLastName(lastName);
+            employee.setDateOfBirth(date);
+            employee.setExperience(experiance);
+
+        }
+    }
+    private static  Date parseDate(String date, String format) throws ParseException,
+            java.text.ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat(format);
+        return formatter.parse(date);
+    }
+
+
 }
--- src/main/java/com/example/teamProjectTwo/service/Impl/XmlFileHandlerServiceImpl.java
+++ src/main/java/com/example/teamProjectTwo/service/Impl/XmlFileHandlerServiceImpl.java
@@ -1,4 +1,7 @@
 package com.example.teamProjectTwo.service.Impl;
 
 public class XmlFileHandlerServiceImpl {
+
+
+
 }
